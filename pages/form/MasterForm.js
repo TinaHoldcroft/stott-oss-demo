9fb55ad2dc93b8a5495c75bf4f6d2908237343a9
Jahn Tore Hansen <jth@blindeforbundet.no>
2021-11-23 13:04:41 +0100

    state sets form default values

--- pages/form/MasterForm.js
+++ pages/form/MasterForm.js
@@ -246,11 +246,11 @@ class MasterForm extends React.Component {
               <fieldset className="fieldset-btn-group">
                   <legend>Velg Frekvens</legend>
                   <div>
-                      <input onChange={props.handleChange} type="radio" id="enGang" name="frekvens" value={props.frekvens}/>
+                      <input onChange={props.handleChange} type="radio" id="enGang" name="frekvens" value='enGang' checked={props.frekvens === 'enGang'}/>
                       <label id="enGangLabel" className="checkBtn float-left" htmlFor="enGang">en gang</label>
                   </div>
                   <div>
-                      <input onChange={props.handleChange} type="radio" id="månedlig" name="frekvens" value="månedlig"/>
+                      <input onChange={props.handleChange} type="radio" id="månedlig" name="frekvens" value="månedlig" checked={props.frekvens === 'månedlig'} />
                       <label className="checkBtn  float-right" htmlFor="månedlig">månedlig</label>
                   </div>
               </fieldset>
@@ -258,19 +258,19 @@ class MasterForm extends React.Component {
               <fieldset className="fieldset-btn-group">
                 <legend>Velg beløp</legend>
                   <div>
-                      <input onChange={props.handleChange} type="radio" id="300" name="beløp" value="300"/>
+                      <input onChange={props.handleChange} type="radio" id="300" name="beløp" value="300" checked={props.beløp === '300'} />
                       <label className="checkBtn float-left" htmlFor="300">300</label>
                   </div>
                   <div>
-                      <input onChange={props.handleChange} type="radio" id="500" name="beløp" value="500"/>
+                      <input onChange={props.handleChange} type="radio" id="500" name="beløp" value="500" checked={props.beløp === '500'} />
                       <label  className="checkBtn float-right" htmlFor="500">500</label>
                   </div>
                   <div>
-                      <input onChange={props.handleChange} type="radio" id="750" name="beløp" value="750"/>
+                      <input onChange={props.handleChange} type="radio" id="750" name="beløp" value="750" checked={props.beløp === '750'} />
                       <label className="checkBtn float-left" htmlFor="750">750</label>
                   </div>
                   <div>
-                      <input onChange={props.handleChange} type="radio" id="1000" name="beløp" value="1000"/>
+                      <input onChange={props.handleChange} type="radio" id="1000" name="beløp" value="1000" checked={props.beløp === '1000'} />
                       <label className="checkBtn  float-right" htmlFor="1000">1000</label>
                   </div>
                   <div className="custom-wrapper">
