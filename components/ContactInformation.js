fb147cba98c5c2179ec615d1de35465a44a3e344
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2022-02-02 13:22:45 +0100

    replaceAll function removed

--- components/ContactInformation.js
+++ components/ContactInformation.js
@@ -4,6 +4,6 @@
 
 //Giverservice
 export const giverPhone = '+47 23 00 06 00'
-export const giverPhoneLink = 'tel:' + giverPhone.replaceAll(' ', '');
+export const giverPhoneLink = 'tel:+4723000600'
 export const giverEmail = 'giver@blindeforbundet.no'
 export const giverEmailLink = 'mailto:giver@blindeforbundet.no'
